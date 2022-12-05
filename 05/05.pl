1a78d07c1d75f1f66cd5170bc0989b4acf84c700
strug62 <strug62@gmail.com>
2022-12-05 15:41:19 +0100

    05 - part2

--- 05/05.pl
+++ 05/05.pl
@@ -1,3 +1,4 @@
+use Clone 'clone';
 while (<>)  {
 	chomp; $line = $_; next unless $_;
 	if ( $_ =~ /move (\d+) from (\d+) to (\d+)/ ) {
@@ -10,12 +11,22 @@ while (<>)  {
 		}
 	}
 }
+@stacks_p1 = @{clone(\@stacks)};
+@stacks_p2 = @{clone(\@stacks)};
 foreach $m (@moves) {
+	@part1 = (); @part2 = ();
 	for (1..$m->[0]) {
-		push @{$stacks[$m->[2]-1]}, pop @{$stacks[$m->[1]-1]};
+		push @part1, pop @{$stacks_p1[$m->[1]-1]};
+		unshift @part2, pop @{$stacks_p2[$m->[1]-1]};
 	}
+	push @{$stacks_p1[$m->[2]-1]}, @part1;
+	push @{$stacks_p2[$m->[2]-1]}, @part2;
 }
-print "part1 : " . join('', map { pop @{$_} } @stacks) . "\n";
+print "part1 : " . join('', map { pop @{$_} } @stacks_p1) . " - part2 : " . join('', map { pop @{$_} } @stacks_p2) . "\n";
+
+
+
+
 
 
 
